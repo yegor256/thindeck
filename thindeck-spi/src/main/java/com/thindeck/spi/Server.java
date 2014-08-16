3a3ae8db44cfb4130086e489a4254c5408562bb9
krzyk <Krzysztof.Krason@gmail.com>
2014-08-16 09:09:14 +0200

    #256 rectructured

--- thindeck-spi/src/main/java/com/thindeck/spi/Server.java
+++ thindeck-spi/src/main/java/com/thindeck/spi/Server.java
@@ -1,9 +1,18 @@
 /**
- * Copyright (c) 2014, thindeck.com
+ * Copyright (c) 2014, Thindeck.com
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are NOT permitted.
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
@@ -18,7 +27,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.thindeck;
+package com.thindeck.spi;
 
 import java.net.InetAddress;
 
