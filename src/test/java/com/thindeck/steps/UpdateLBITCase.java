42bc7fd2617446d5a4f19e7e4c06978147343d17
Carlos Miranda <carlosmiranda@users.noreply.github.com>
2014-09-12 12:06:05 +0800

    #293 UpdateLB stub and test case

---
+++ src/test/java/com/thindeck/steps/UpdateLBITCase.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2014, Thindeck.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the thindeck.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.thindeck.steps;
+
+import com.jcabi.ssh.SSH;
+import com.jcabi.ssh.Shell;
+import com.thindeck.api.Context;
+import com.thindeck.api.Step;
+import com.thindeck.api.mock.MkContext;
+import java.io.IOException;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Assume;
+import org.junit.Test;
+import org.xembly.Directives;
+
+/**
+ * Integration test for {@link UpdateLB}.
+ *
+ * @author Carlos Miranda (miranda.cma@gmail.com)
+ * @version $Id$
+ */
+public final class UpdateLBITCase {
+
+    /**
+     * FindTanks can update load balancer configuration from memo.
+     * @throws IOException If fails
+     * @todo #293 Let's implement UpdateLB. What this step needs to do is to
+     *  update the load balancer configuration on the load balancer(s). We are
+     *  assuming, for the moment, that Nginx is used. In this case, UpdateLB
+     *  should SSH to the load balancer and update its nginx.conf. This test
+     *  will check whether the configuration of a server with Nginx installed is
+     *  updated after UpdateLB is executed. For more details, see
+     *  https://github.com/yegor256/thindeck/issues/303
+     */
+    @Test
+    @org.junit.Ignore
+    public void updatesLoadBalancerNginxConfig() throws IOException {
+        // @checkstyle MultipleStringLiterals (5 lines)
+        Assume.assumeNotNull(
+            System.getProperty("failsafe.lb.ssh.location"),
+            System.getProperty("failsafe.lb.ssh.port"),
+            System.getProperty("failsafe.lb.ssh.user"),
+            System.getProperty("failsafe.lb.ssh.key")
+        );
+        final Step step = new UpdateLB();
+        final Context ctx = new MkContext();
+        ctx.memo().update(
+            new Directives()
+                .xpath("/memo/containers")
+                .addIf("container")
+                .add("tank").set("tank.fake").up()
+        );
+        step.exec(ctx);
+        final Shell ssh = new SSH(
+            System.getProperty("failsafe.lb.ssh.location"),
+            Integer.valueOf(System.getProperty("failsafe.lb.ssh.port")),
+            System.getProperty("failsafe.lb.ssh.user"),
+            System.getProperty("failsafe.lb.ssh.key")
+        );
+        MatcherAssert.assertThat(
+            new Shell.Plain(ssh).exec(
+                "cat /usr/local/nginx/conf/nginx.conf"
+            ),
+            Matchers.containsString("server tank.fake")
+        );
+    }
+
+}
