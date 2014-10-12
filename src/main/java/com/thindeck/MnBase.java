2dc3fb0ab79758857886f27ba89dfa891b751eaa
krzyk <Krzysztof.Krason@gmail.com>
2014-10-12 21:06:50 +0200

    #326 removed cache from memo

--- src/main/java/com/thindeck/MnBase.java
+++ src/main/java/com/thindeck/MnBase.java
@@ -29,7 +29,6 @@
  */
 package com.thindeck;
 
-import com.jcabi.aspects.Cacheable;
 import com.jcabi.aspects.Immutable;
 import com.jcabi.urn.URN;
 import com.thindeck.api.Base;
@@ -69,7 +68,6 @@ public final class MnBase implements Base {
      * @return Memo
      * @throws IOException If fails
      */
-    @Cacheable(forever = true)
     public static Memo memo() throws IOException {
         return new MkMemo();
     }
