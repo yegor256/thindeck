5baf362e762083befc1acd2af72b0d859cf5b01d
longtimeago <ppol@ua.fm>
2014-10-15 23:22:51 +0300

    #310 Test for adding repo

---
+++ src/test/casperjs/AddRepoTest.js
@@ -0,0 +1,27 @@
+/*globals casper:false */
+casper.test.begin(
+    'can add repo',
+    function (test) {
+        casper.start(
+            casper.cli.get("home") + '/repos',
+            function () {
+                test.assertHttpStatus(200);
+            }
+        );
+
+        casper.then(
+            function () {
+                this.fillForm('.content > form', {
+                    'name' : 'test_repo_name',
+                    'uri'  : 'test_repo_uri'
+                });
+                test.assertHttpStatus(200);
+            }
+        );
+        casper.run(
+            function () {
+                test.done();
+            }
+        );
+    }
+);
