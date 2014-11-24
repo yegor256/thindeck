5865927e736278e9118d2b96955fecf442eba242
piotrkot <piotr.kotlicki@gmail.com>
2014-11-24 11:57:58 +0100

    Revert "#372 Implementation of txn method"
    This reverts commit 83d8c088ac2e94df708d409bbec9ffb5c4f95cc8.

--- src/main/java/com/thindeck/dynamo/DyBase.java
+++ src/main/java/com/thindeck/dynamo/DyBase.java
@@ -44,6 +44,7 @@ import com.thindeck.api.User;
  * @author Krzyszof Krason (Krzysztof.Krason@gmail.com)
  * @version $Id$
  * @since 0.3
+ * @todo #322 Implement txn method to retrieve data from appropriate table.
  */
 public final class DyBase implements Base {
     /**
@@ -80,15 +81,6 @@ public final class DyBase implements Base {
 
     @Override
     public Txn txn(final Task task) {
-        return new DyTxn(
-            this.region.table(DyTxn.TBL)
-                .frame()
-                .through(
-                    new QueryValve()
-                        .withLimit(1)
-                )
-                .where(DyTxn.ATTR_ID, Long.toString(task.number()))
-                .iterator().next()
-        );
+        throw new UnsupportedOperationException();
     }
 }
