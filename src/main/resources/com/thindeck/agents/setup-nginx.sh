cd25e27c2261fb5596939c1df2d0d41664b140dd
Yegor Bugayenko <yegor@tpc2.com>
2015-07-28 07:59:56 +0200

    typo

--- src/main/resources/com/thindeck/agents/setup-nginx.sh
+++ src/main/resources/com/thindeck/agents/setup-nginx.sh
@@ -5,8 +5,7 @@ sudo mkdir -p /etc/nginx/conf.d/thindeck
 
 tmp=$(mktemp)
 echo "http { include /etc/nginx/conf.d/thindeck/*.conf; }" > "${tmp}"
-
-sudo mv "${tmp}" /etc/nginx/conf.d
+sudo mv "${tmp}" /etc/nginx/conf.d/thindeck.conf
 
 echo "*/5 * * * * sudo service nginx reload" > "${tmp}"
 crontab "${tmp}"
