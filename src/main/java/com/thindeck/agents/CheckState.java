11b8ec043feea489551d7d03bf592ae73ef4f58d
Yegor Bugayenko <yegor@tpc2.com>
2015-07-28 20:52:23 +0200

    typo

--- src/main/java/com/thindeck/agents/CheckState.java
+++ src/main/java/com/thindeck/agents/CheckState.java
@@ -74,7 +74,7 @@ public final class CheckState implements Agent {
     public Iterable<Directive> exec(final XML deck) throws IOException {
         final Directives dirs = new Directives();
         final Collection<XML> containers = deck.nodes(
-            "/deck/containers/container[http]"
+            "/deck/containers/container[host and http]"
         );
         for (final XML ctr : containers) {
             final Integer port = Integer.parseInt(
