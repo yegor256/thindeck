1ef3ea976ec393a70c226307c6419d799db9b7a7
Yegor Bugayenko <yegor@tpc2.com>
2015-07-29 06:17:03 +0200

    delete nginx file if no servers

--- src/main/resources/com/thindeck/agents/update-nginx.sh
+++ src/main/resources/com/thindeck/agents/update-nginx.sh
@@ -3,21 +3,24 @@ set -e
 
 tmp=$(mktemp)
 
-echo " \
-upstream ${group} { \
-  ${servers}
-} \
-server { \
-  listen ${domain}:${port}; \
-  server_name ${domain}; \
-  location / { \
-    proxy_set_header X-Real-IP \$remote_addr;
-    proxy_set_header X-Forwarded-For \$remote_addr;
-    proxy_set_header Host \$host;
-    proxy_pass http://${group}; \
+if [ -n "${servers}" ]; then
+  echo " \
+  upstream ${group} { \
+    ${servers}
   } \
-}" > "${tmp}"
-
-sudo mv "${tmp}" "/etc/nginx/conf.d/thindeck/${domain}.conf"
-sudo chown nginx "/etc/nginx/conf.d/thindeck/${domain}.conf"
-sudo chmod 644 "/etc/nginx/conf.d/thindeck/${domain}.conf"
+  server { \
+    listen ${domain}:${port}; \
+    server_name ${domain}; \
+    location / { \
+      proxy_set_header X-Real-IP \$remote_addr;
+      proxy_set_header X-Forwarded-For \$remote_addr;
+      proxy_set_header Host \$host;
+      proxy_pass http://${group}; \
+    } \
+  }" > "${tmp}"
+  sudo mv "${tmp}" "/etc/nginx/conf.d/thindeck/${domain}.conf"
+  sudo chown nginx "/etc/nginx/conf.d/thindeck/${domain}.conf"
+  sudo chmod 644 "/etc/nginx/conf.d/thindeck/${domain}.conf"
+else
+  sudo rm -f "/etc/nginx/conf.d/thindeck/${domain}.conf"
+fi
