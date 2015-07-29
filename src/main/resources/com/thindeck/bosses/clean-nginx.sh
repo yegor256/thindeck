59015d4e99a9779e62865100ddd9ddd687559b5e
Yegor Bugayenko <yegor@tpc2.com>
2015-07-29 17:48:48 +0200

    log

--- src/main/resources/com/thindeck/bosses/clean-nginx.sh
+++ src/main/resources/com/thindeck/bosses/clean-nginx.sh
@@ -4,6 +4,7 @@ set -e
 dir=/etc/nginx/conf.d/thindeck
 for i in $(ls ${dir}/*.conf); do
   if ! grep "${i}" ~/domains; then
-    sudo rm -rf ${dir}/${i}
+    sudo rm -rf "${dir}/${i}"
+    echo "Removed Nginx config: ${dir}/${i}"
   fi
 done
