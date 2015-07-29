8830cecbbe4b21e9ef71a06ca7d1bd1e50014cde
Yegor Bugayenko <yegor@tpc2.com>
2015-07-29 05:27:12 +0200

    #544 ruby demo page

---
+++ demo-docker-images/ruby-sinatra/README.md
@@ -0,0 +1,4 @@
+It's a simple Docker image that runs Sinatra on Ruby on Ubuntu 14.
+
+It is online here: [ruby.demo.thindeck.com](http://ruby.demo.thindeck.com)
+
---
+++ demo-docker-images/ruby-sinatra/app/hello.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+
+get '/' do
+  "<html>
+    <body>
+      <p>Hello, world!</p>
+      <p>Ruby version is #{RUBY_VERSION}</p>
+      <p>This page is hosted by <a href='http://www.thindeck.com'>thindeck.com</a>.</p>
+      <p>Source code is in <a href='https://github.com/yegor256/thindeck/tree/master/demo-docker-images/ruby-sinatra'>Github</a>.</p>
+    </body>
+  </html>"
+end
