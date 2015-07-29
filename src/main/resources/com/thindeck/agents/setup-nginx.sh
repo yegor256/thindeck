26f116afe831469e2a003b89bd53563d2c30f42a
Yegor Bugayenko <yegor@tpc2.com>
2015-07-29 12:27:23 +0200

    return 444

--- src/main/resources/com/thindeck/agents/setup-nginx.sh
+++ src/main/resources/com/thindeck/agents/setup-nginx.sh
@@ -6,7 +6,13 @@ sudo setenforce 0
 sudo mkdir -p /etc/nginx/conf.d/thindeck
 
 tmp=$(mktemp)
-echo "include /etc/nginx/conf.d/thindeck/*.conf;" > "${tmp}"
+echo "include /etc/nginx/conf.d/thindeck/*.conf;
+  server {
+    listen 80;
+    server_name \"\";
+    return 444;
+  }
+" > "${tmp}"
 sudo mv "${tmp}" /etc/nginx/conf.d/thindeck.conf
 sudo chown nginx /etc/nginx/conf.d/thindeck.conf
 sudo chmod 644 /etc/nginx/conf.d/thindeck.conf
--- src/main/resources/com/thindeck/agents/update-nginx.sh
+++ src/main/resources/com/thindeck/agents/update-nginx.sh
@@ -4,19 +4,19 @@ set -e
 tmp=$(mktemp)
 
 if [ -n "${servers}" ]; then
-  echo " \
-  upstream ${group} { \
+  echo "
+  upstream ${group} {
     ${servers}
-  } \
-  server { \
-    listen ${domain}:${port}; \
-    server_name ${domain}; \
-    location / { \
+  }
+  server {
+    listen ${domain}:${port};
+    server_name ${domain};
+    location / {
       proxy_set_header X-Real-IP \$remote_addr;
       proxy_set_header X-Forwarded-For \$remote_addr;
       proxy_set_header Host \$host;
-      proxy_pass http://${group}; \
-    } \
+      proxy_pass http://${group};
+    }
   }" > "${tmp}"
   sudo mv "${tmp}" "/etc/nginx/conf.d/thindeck/${domain}.conf"
   sudo chown nginx "/etc/nginx/conf.d/thindeck/${domain}.conf"
