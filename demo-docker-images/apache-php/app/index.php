ed368dc7d44421f85daa111be28ed482e30833f1
Yegor Bugayenko <yegor@tpc2.com>
2015-07-29 05:15:42 +0200

    link to github

--- demo-docker-images/apache-php/app/index.php
+++ demo-docker-images/apache-php/app/index.php
@@ -2,5 +2,7 @@
   <body>
     <p>Hello, world!</p>
     <p>PHP version is <?php echo phpversion(); ?></p>
+    <p>This page is hosted by <a href="http://www.thindeck.com">thindeck.com</a>.</p>
+    <p>Source code is in <a href="https://github.com/yegor256/thindeck/tree/master/demo-docker-images/apache-php">Github</a>.</p>
   </body>
 </html>
