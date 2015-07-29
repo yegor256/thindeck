da71f0d8af48c26f398a3582f3a4e92e2a7f3374
Yegor Bugayenko <yegor@tpc2.com>
2015-07-29 22:21:16 +0200

    log

--- src/main/resources/com/thindeck/bosses/clean-nginx.sh
+++ src/main/resources/com/thindeck/bosses/clean-nginx.sh
@@ -3,6 +3,9 @@ set -e
 
 dir=/etc/nginx/conf.d/thindeck
 mkdir -p "${dir}"
+
+echo "There are $(wc ~/domains) legitimate"
+
 for i in $(ls "${dir}"); do
   if ! grep -q "${i}" ~/domains; then
     sudo rm -rf "${dir}/${i}"
