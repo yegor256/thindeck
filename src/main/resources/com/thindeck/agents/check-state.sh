f0baef83d3b1f007c1f2832077028f8c2a35f059
Yegor Bugayenko <yegor@tpc2.com>
2015-07-29 21:23:55 +0200

    log fix

--- src/main/resources/com/thindeck/agents/check-state.sh
+++ src/main/resources/com/thindeck/agents/check-state.sh
@@ -2,7 +2,7 @@
 
 target="localhost:${port}"
 if curl --silent --show-error --fail "${target}" > /dev/null; then
-  echo "${target} is ALIVE"
+  echo "${target} is ALIVE" > /dev/null
 else
   echo "${target} is dead"
 fi
