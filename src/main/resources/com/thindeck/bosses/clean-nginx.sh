a0f6b65c816adfc828416f86c56e7ff96a3cdf6c
Yegor Bugayenko <yegor@tpc2.com>
2015-07-29 22:51:47 +0200

    log

--- src/main/resources/com/thindeck/bosses/clean-nginx.sh
+++ src/main/resources/com/thindeck/bosses/clean-nginx.sh
@@ -4,11 +4,11 @@ set -e
 dir=/etc/nginx/conf.d/thindeck
 mkdir -p "${dir}"
 
-echo "There are $(wc ~/domains) legitimate"
+echo "There are $(wc -l ~/domains) legitimate"
 
 for i in $(ls "${dir}"); do
   if ! grep -q "${i}" ~/domains; then
+    echo "Removing Nginx config: ${dir}/${i}"
     sudo rm -rf "${dir}/${i}"
-    echo "Removed Nginx config: ${dir}/${i}"
   fi
 done
