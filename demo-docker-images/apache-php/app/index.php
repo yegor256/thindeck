510f47e0608ab4f647c5a78327dfe863fd36c903
Yegor Bugayenko <yegor@tpc2.com>
2015-08-01 20:15:32 +0300

    short tag

--- demo-docker-images/apache-php/app/index.php
+++ demo-docker-images/apache-php/app/index.php
@@ -1,8 +1,8 @@
 <html>
   <body>
     <p>Hello, world!</p>
-    <p>PHP version is <?php echo phpversion(); ?></p>
+    <p>PHP version is <? echo phpversion(); ?></p>
     <p>This page is hosted by <a href="http://www.thindeck.com">thindeck.com</a>.</p>
-    <p>Source code is in <a href="https://github.com/yegor256/thindeck/tree/master/demo-docker-images/apache-php">Github</a>.</p>
+    <p>Its source code is in <a href="https://github.com/yegor256/thindeck/tree/master/demo-docker-images/apache-php">Github</a>.</p>
   </body>
 </html>
